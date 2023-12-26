d73e9b93cd8991c5b7b21b7ce0ad382f018ce5dc
patrol114 <angonpl@hotmail.com>
2023-12-27 00:23:38 +0100

    Add files via upload
    Signed-off-by: patrol114 <angonpl@hotmail.com>

---
+++ ChatBotYeti/main.py
@@ -0,0 +1,53 @@
+# Imports
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from llama import Llama
+from flask import Flask, request, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+# Model Initialization
+
+# Load pre-trained GPT-2 model
+gpt2_tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+gpt2_model = GPT2LMHeadModel.from_pretrained("gpt2")
+llama_tokenizer = gpt2_tokenizer
+llama = Llama("lstm-large", tokenizer=llama_tokenizer)
+
+
+# Flask App Initialization
+app = Flask(__name__)
+limiter = Limiter(app=app, key_func=get_remote_address)
+
+# Chatbot Functionality
+@app.route("/chatbot", methods=["POST"])
+@limiter.limit("5 per minute")  # Rate limiting
+def chat():
+    try:
+        data = request.get_json()
+        user_input = data.get("user_input")
+        if user_input is None:
+            return jsonify({"error": "User input is missing."}), 400
+
+        bot_response = get_response(user_input)
+        return jsonify({"response": bot_response})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# Function to generate bot response
+def get_response(user_input):
+    # Get LLama response
+    llama_resp = llama.predict(user_input)
+
+    # Generate GPT-2 response
+    gpt2_input = gpt2_tokenizer.encode(llama_resp, return_tensors="pt")
+    gpt2_output = gpt2_model.generate(gpt2_input, max_length=100)
+
+    # Post-process GPT-2 response
+    response = gpt2_tokenizer.decode(gpt2_output[0], skip_special_tokens=True)
+    return response
+
+# Execution
+if __name__ == "__main__":
+    app.run(debug=True)
