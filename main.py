30abd770b9eefc01ca497a49f818dae4fd588448
patrol114 <angonpl@hotmail.com>
2023-12-27 22:41:21 +0100

    Update main.py
    Signed-off-by: patrol114 <angonpl@hotmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+import torch
+from transformers import AutoTokenizer, GPT2LMHeadModel
+from flask import Flask, request, render_template
+from flask_limiter import Limiter
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from re import sub
+
+# Set up Flask app and Limiter
+app = Flask(__name__)
+limiter = Limiter(app)
+port = 9875
+
+# Set up models and tokenizers
+gpt2_model = GPT2LMHeadModel.from_pretrained('gpt2')
+gpt2_tokenizer = AutoTokenizer.from_pretrained('gpt2')
+llama_tokenizer = AutoTokenizer.from_pretrained('jarradh/llama2_70b_chat_uncensored')
+
+# Define a function to generate responses using both models
 def generate_response(user_input):
     # Check if llama_tokenizer is not None
     if llama_tokenizer is not None:
@@ -27,3 +47,21 @@ def generate_response(user_input):
     else:
         response = None
     return response
+
+# Set up routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/chatbot', methods=['GET', 'POST'])
+@limiter.limit("5 per minute")  # Rate limiting
+def chatbot():
+    if request.method == 'POST':
+        user_input = request.form.get('message')
+        response = generate_response(user_input)
+        return response
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=port, debug=True)
