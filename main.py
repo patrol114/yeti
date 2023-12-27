9e38f9d817bdcbcea790e8237178027f2e42af7b
patrol114 <angonpl@hotmail.com>
2023-12-27 16:22:39 +0100

    Update main.py
    Signed-off-by: patrol114 <angonpl@hotmail.com>

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ limiter = Limiter(app)
 # Set up models and tokenizers
 gpt2_model = GPT2LMHeadModel.from_pretrained('gpt2')
 gpt2_tokenizer = AutoTokenizer.from_pretrained('gpt2')
-llama_tokenizer = AutoTokenizer.from_pretrained('llama')
+llama_tokenizer = AutoTokenizer.from_pretrained('Nelsonxx1/llama-2-70b-chat')
 
 # Define a function to generate responses using both models
 def generate_response(user_input):
