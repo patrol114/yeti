970d3ea9bb6bf519c881e251bec64bc6d40cd68a
patrol114 <angonpl@hotmail.com>
2023-12-27 22:28:53 +0100

    Update main.py
    Signed-off-by: patrol114 <angonpl@hotmail.com>

--- main.py
+++ main.py
@@ -16,48 +16,39 @@ port = 9875
 gpt2_model = GPT2LMHeadModel.from_pretrained('gpt2')
 gpt2_tokenizer = AutoTokenizer.from_pretrained('gpt2')
 llama_tokenizer = AutoTokenizer.from_pretrained('jarradh/llama2_70b_chat_uncensored')
-import torch
-from transformers import AutoTokenizer, GPT2LMHeadModel
-from flask import Flask, request, render_template
-from flask_limiter import Limiter
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from re import sub
-
-# Set up Flask app and Limiter
-app = Flask(__name__)
-limiter = Limiter(app)
-port = 9875
-
-# Set up models and tokenizers
-gpt2_model = GPT2LMHeadModel.from_pretrained('gpt2')
-gpt2_tokenizer = AutoTokenizer.from_pretrained('gpt2')
-llama_tokenizer = AutoTokenizer.from_pretrained('jarradh/llama2_70b_chat_uncensored')
 
 # Define a function to generate responses using both models
 def generate_response(user_input):
-    # Preprocess user input
-    user_input = sub(r'[^\w\s]', '', user_input)
-    user_input = user_input.lower()
-    tokens = word_tokenize(user_input)
-    stop_words = set(stopwords.words('english'))
-    tokens = [token for token in tokens if token not in stop_words]
+    # Check if llama_tokenizer is not None
+    if llama_tokenizer is not None:
+        # Preprocess user input
+        user_input = sub(r'[^\w\s]', '', user_input)
+        user_input = user_input.lower()
+        tokens = word_tokenize(user_input)
+        stop_words = set(stopwords.words('english'))
+        tokens = [token for token in tokens if token not in stop_words]
 
-    # Generate response using LLM
-    llama_input = " ".join(tokens)
-    llama_output = llama_tokenizer(llama_input, return_tensors="pt", truncation=True, max_length=512)
-    llama_preds = gpt2_model.generate(llama_output['input_ids'], max_length=512, num_return_sequences=1)
-    llama_output = gpt2_tokenizer.decode(llama_preds[0], skip_special_tokens=True)
+        # Generate response using LLM
+        llama_input = " ".join(tokens)
+        if llama_tokenizer is not None:
+            llama_output = llama_tokenizer(llama_input, return_tensors="pt", truncation=True, max_length=512)
+            if llama_output is not None:
+                llama_output = llama_output.tolist()
 
-    # Generate response using GPT-2
-    gpt2_input = " ".join(tokens)
-    gpt2_output = gpt2_tokenizer(gpt2_input, return_tensors="pt", truncation=True, max_length=512)
-    gpt2_preds = gpt2_model.generate(gpt2_output['input_ids'], max_length=512, num_return_sequences=1)
-    gpt2_output = gpt2_tokenizer.decode(gpt2_preds[0], skip_special_tokens=True)
+        # Generate response using GPT-2
+        gpt2_input = " ".join(tokens)
+        if gpt2_input is not None:
+            gpt2_output = gpt2_tokenizer(gpt2_input, return_tensors="pt", truncation=True, max_length=512)
+            if gpt2_output is not None:
+                gpt2_output = gpt2_output.tolist()
 
-    # Combine and return the final response
-    response = llama_output + " " + gpt2_output
+        # Combine and return the final response
+        if llama_output is not None and gpt2_output is not None:
+            response = llama_output + " " + gpt2_output
+        else:
+            response = None
+    else:
+        response = None
     return response
 
 # Set up routes
