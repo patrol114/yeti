eb825cf2878d56725f650ae7f221a96d631209ed
patrol114 <angonpl@hotmail.com>
2023-12-27 22:46:14 +0100

    Update main.py
    Signed-off-by: patrol114 <angonpl@hotmail.com>

--- main.py
+++ main.py
@@ -19,27 +19,27 @@ llama_tokenizer = AutoTokenizer.from_pretrained('jarradh/llama2_70b_chat_uncenso
 
 # Define a function to generate responses using both models
 def generate_response(user_input):
-    # Check if llama_tokenizer is not None
+    # Sprawdź, czy llama_tokenizer nie jest None
     if llama_tokenizer is not None:
-        # Preprocess user input
+        # Przetwórz wejście od użytkownika
         user_input = sub(r'[^\w\s]', '', str(user_input))  # Dodaj konwersję do ciągu
         user_input = user_input.lower()
         tokens = word_tokenize(user_input)
         stop_words = set(stopwords.words('english'))
         tokens = [token for token in tokens if token not in stop_words]
 
-        # Convert tokens back to string
+        # Konwertuj tokeny z powrotem do ciągu
         user_input_str = " ".join(tokens)
 
-        # Generate response using LLM
+        # Generuj odpowiedź przy użyciu LLM
         llama_input = user_input_str
         if llama_tokenizer is not None:
             llama_output = llama_tokenizer(llama_input, return_tensors="pt", truncation=True, max_length=512)
             if llama_output is not None and 'input_ids' in llama_output:
-                # Use 'input_ids' as input to GPT-2 model
+                # Użyj 'input_ids' jako wejścia do modelu GPT-2
                 llama_input_ids = llama_output['input_ids']
                 gpt2_output = gpt2_model.generate(llama_input_ids, max_length=512, num_return_sequences=1)
-                response = gpt2_output[0]
+                response = gpt2_output[0].decode('utf-8')  # Dekoduj tensor do postaci tekstu
             else:
                 response = None
         else:
