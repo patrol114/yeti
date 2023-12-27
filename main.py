61c7fe32daf17c5bbb8241818b5dfa06545dfb50
patrol114 <angonpl@hotmail.com>
2023-12-27 22:40:10 +0100

    Update main.py
    Signed-off-by: patrol114 <angonpl@hotmail.com>

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-import torch
-from transformers import AutoTokenizer, GPT2LMHeadModel
-from flask import Flask, request, render_template
-from flask_limiter import Limiter
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from re import sub
-
-# Set up Flask app and Limiter
-app = Flask(__name__)
-limiter = Limiter(app)
-port = 9875
-
-# Set up models and tokenizers
-gpt2_model = GPT2LMHeadModel.from_pretrained('gpt2')
-gpt2_tokenizer = AutoTokenizer.from_pretrained('gpt2')
-llama_tokenizer = AutoTokenizer.from_pretrained('jarradh/llama2_70b_chat_uncensored')
-
-# Define a function to generate responses using both models
 def generate_response(user_input):
     # Check if llama_tokenizer is not None
     if llama_tokenizer is not None:
@@ -35,39 +15,15 @@ def generate_response(user_input):
         llama_input = user_input_str
         if llama_tokenizer is not None:
             llama_output = llama_tokenizer(llama_input, return_tensors="pt", truncation=True, max_length=512)
-            if llama_output is not None:
-                llama_output = llama_output["input_ids"].numpy()
-
-        # Generate response using GPT-2
-        gpt2_input = user_input_str
-        if gpt2_input is not None:
-            gpt2_output = gpt2_tokenizer(gpt2_input, return_tensors="pt", truncation=True, max_length=512)
-            if gpt2_output is not None:
-                gpt2_output = gpt2_output.tolist()
-
-        # Combine and return the final response
-        if llama_output is not None and gpt2_output is not None:
-            response = llama_output + " " + gpt2_output
+            if llama_output is not None and 'input_ids' in llama_output:
+                # Use 'input_ids' as input to GPT-2 model
+                llama_input_ids = llama_output['input_ids']
+                gpt2_output = gpt2_model.generate(llama_input_ids, max_length=512, num_return_sequences=1)
+                response = gpt2_output[0]
+            else:
+                response = None
         else:
             response = None
     else:
         response = None
     return response
-
-# Set up routes
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/chatbot', methods=['GET', 'POST'])
-@limiter.limit("5 per minute")  # Rate limiting
-def chatbot():
-    if request.method == 'POST':
-        user_input = request.form.get('message')
-        response = generate_response(user_input)
-        return response
-    else:
-        return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(port=port, debug=True)
