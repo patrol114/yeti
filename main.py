0a788b2c3e80906550c095cccbd8fc924fff474f
patrol114 <angonpl@hotmail.com>
2023-12-27 23:03:45 +0100

    Update main.py
    Signed-off-by: patrol114 <angonpl@hotmail.com>

--- main.py
+++ main.py
@@ -18,27 +18,27 @@ gpt2_tokenizer = AutoTokenizer.from_pretrained('gpt2')
 llama_tokenizer = AutoTokenizer.from_pretrained('jarradh/llama2_70b_chat_uncensored')
 
 def generate_response(user_input):
-    # Sprawdź, czy llama_tokenizer nie jest None
+    # Check if llama_tokenizer is not None
     if llama_tokenizer is not None:
-        # Przetwórz wejście od użytkownika
-        user_input = sub(r'[^\w\s]', '', str(user_input))  # Dodaj konwersję do ciągu
+        # Process user input
+        user_input = sub(r'[^\w\s]', '', str(user_input))  # Add conversion to string
         user_input = user_input.lower()
         tokens = word_tokenize(user_input)
         stop_words = set(stopwords.words('english'))
         tokens = [token for token in tokens if token not in stop_words]
 
-        # Konwertuj tokeny z powrotem do ciągu
+        # Convert tokens back to string
         user_input_str = " ".join(tokens)
 
-        # Generuj odpowiedź przy użyciu LLM
+        # Generate response using LLM
         llama_input = user_input_str
         if llama_tokenizer is not None:
             llama_output = llama_tokenizer(llama_input, return_tensors="pt", truncation=True, max_length=512)
             if llama_output is not None and 'input_ids' in llama_output:
-                # Użyj 'input_ids' jako wejścia do modelu GPT-2
+                # Use 'input_ids' as input to GPT-2 model
                 llama_input_ids = llama_output['input_ids']
                 gpt2_output = gpt2_model.generate(llama_input_ids, max_length=512, num_return_sequences=1)
-                response = str(gpt2_output[0].item())  # Pobierz wartość tensora jako zwykłą liczbę Pythona i konwertuj na ciąg znaków
+                response = gpt2_tokenizer.decode(gpt2_output[0], skip_special_tokens=True)  # Decode the tensor to text
             else:
                 response = None
         else:
