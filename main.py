61fc7c03d2601f4eb9c8b56edc657ef364543a43
patrol114 <angonpl@hotmail.com>
2023-12-27 22:54:13 +0100

    Update main.py
    Signed-off-by: patrol114 <angonpl@hotmail.com>

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-import torch
-from transformers import AutoTokenizer, GPT2LMHeadModel
-from flask import Flask, request, render_template
-from flask_limiter import Limiter
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from re import sub
-
-# Set up Flask app and Limiter
-app = Flask(__name__)
-limiter = Limiter(app)
-port = 9875
-
-# Set up models and tokenizers
-gpt2_model = GPT2LMHeadModel.from_pretrained('gpt2')
-gpt2_tokenizer = AutoTokenizer.from_pretrained('gpt2')
-llama_tokenizer = AutoTokenizer.from_pretrained('jarradh/llama2_70b_chat_uncensored')
-
-# Define a function to generate responses using both models
 def generate_response(user_input):
     # Sprawdź, czy llama_tokenizer nie jest None
     if llama_tokenizer is not None:
@@ -39,7 +19,7 @@ def generate_response(user_input):
                 # Użyj 'input_ids' jako wejścia do modelu GPT-2
                 llama_input_ids = llama_output['input_ids']
                 gpt2_output = gpt2_model.generate(llama_input_ids, max_length=512, num_return_sequences=1)
-                response = str(gpt2_output[0].decode('utf-8'))  # Dekoduj tensor do postaci tekstu i następnie konwertuj na ciąg znaków
+                response = str(gpt2_output[0].item())  # Pobierz wartość tensora jako zwykłą liczbę Pythona i konwertuj na ciąg znaków
             else:
                 response = None
         else:
@@ -47,21 +27,3 @@ def generate_response(user_input):
     else:
         response = None
     return response
-
-# Set up routes
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/chatbot', methods=['GET', 'POST'])
-@limiter.limit("5 per minute")  # Rate limiting
-def chatbot():
-    if request.method == 'POST':
-        user_input = request.form.get('message')
-        response = generate_response(user_input)
-        return response
-    else:
-        return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(port=port, debug=True)
